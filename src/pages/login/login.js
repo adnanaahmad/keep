9e50b41fa6d68c3ada41f0330d50f4436e06414a
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-03-08 00:23:39 +0500

    sign in page ready

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -6,28 +6,41 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import classes from './login.module.css'
+import classes from './login.module.css';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import {toggleBorder} from '../../shared/styles/debugging-border';
 
 function Login() {
-  return (
-      <React.Fragment>
-        <Card sx={{ width: 400, height: 260, alignSelf: 'center' }} className={classes.login}>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div" sx={{ mb: 2 }}>
-                Notes
-                </Typography>
-                <Stack component="form" spacing={2}>
-                    <TextField fullWidth label="User Name" id="fullWidth" />
-                    <TextField fullWidth label="Password" id="fullWidth" />
-                </Stack>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Login</Button>
-                <Button size="small">Register</Button>
-            </CardActions>
-        </Card>
-      </React.Fragment>
-  );
+    const isBorder = toggleBorder;
+    return (
+        <React.Fragment>
+            <Stack justifyContent={'center'} alignItems={'center'} sx={{border: isBorder ? '2px solid red' : 'none', height: '100vh', paddingX:'.5rem'}}>
+                <Card sx={{ width:'100%', maxWidth: 400, height: 'fit-content', boxShadow: 3 }} className={classes.login}>
+                    <CardContent sx={{border: isBorder ? '1px solid blue' : 'none', mb:3}}>
+                        <Stack alignItems={'center'} sx={{ mb: 4, border: isBorder ? '1px solid orange' : 'none' }} spacing={'1.2rem'}>
+                            <AutoStoriesIcon sx={{marginX: 'auto', display:'flex', fontSize: '3rem'}} color="primary"/>
+                            <Stack>
+                                <Typography gutterBottom variant="h5" component="div" textAlign={'center'}>
+                                    Sign in
+                                </Typography>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom component="div" textAlign={'center'}>
+                                    to continue to notes
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                        <Stack component="form" spacing={2}>
+                            <TextField fullWidth label="Email" id="fullWidth" />
+                            <TextField fullWidth label="Password" id="fullWidth" />
+                        </Stack>
+                    </CardContent>
+                    <CardActions sx={{border: isBorder ? '1px solid pink' : 'none', paddingBottom:'1rem'}}>
+                        <Button variant='contained' size="small">Next</Button>
+                        <Button size="small">Register</Button>
+                    </CardActions>
+                </Card>
+            </Stack>
+        </React.Fragment>
+    );
 }
 
 export default Login;
