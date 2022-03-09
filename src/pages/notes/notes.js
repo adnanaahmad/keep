04affd1d5d68c4d17bebc8400be58816fe571300
adnanahmad95 <adnan.ahmad@narsun.pk>
2022-03-09 14:36:35 +0500

    react draggable installed

--- src/pages/notes/notes.js
+++ src/pages/notes/notes.js
@@ -1,23 +1,3 @@
-// import GridLayout from 'react-grid-layout';
-// import '../../../node_modules/react-grid-layout/css/styles.css';
-// import '../../../node_modules/react-resizable/css/styles.css';
-// import './notes.css'
-// function Notes () {
-//     // layout is an array of objects, see the demo for more complete usage
-//     const layout = [
-//       {i: 'a', x: 0, y: 0, w: 2, h: 2, minW: 2, maxW: 2},
-//       {i: 'b', x: 2, y: 0, w: 2, h: 2, minW: 2, maxW: 2},
-//       {i: 'c', x: 4, y: 0, w: 2, h: 2, minW: 2, maxW: 2}
-//     ];
-//     return (
-//       <GridLayout className="layout" layout={layout} cols={6} rowHeight={30} width={1200}>
-//         <div key="a">a</div>
-//         <div key="b">b</div>
-//         <div key="c">c</div>
-//       </GridLayout>
-//     )
-// }
-// export default Notes;
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
