255c182337067fb0e71216cb0eb785bc60ec20ea
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-03-11 14:44:53 +0500

    note class added

--- src/pages/nav/notes/notes.js
+++ src/pages/nav/notes/notes.js
@@ -3,16 +3,15 @@ import Draggable from 'react-draggable';
 import Box from '@mui/material/Box';
 import { toggleBorder } from '../../../shared/styles/debugging-border';
 
-export default class Notes extends React.Component {
+class Note extends React.Component {
   state = {
     activeDrags: 0,
     deltaPosition: {
       x: 0, y: 0
     },
-    controlledPosition: {
-      x: -400, y: 200
-    }
   };
+
+  // event handlers
 
   handleDrag = (e, ui) => {
     const {x, y} = this.state.deltaPosition;
@@ -25,26 +24,21 @@ export default class Notes extends React.Component {
   };
 
   onStart = () => {
-    //this.setState({activeDrags: ++this.state.activeDrags});
     this.setState(prev => {
       return {
         activeDrags: prev.activeDrags + 1,
       };
     });
-    //console.log(this.state.deltaPosition);
   };
 
   onStop = () => {
-    //this.setState({activeDrags: --this.state.activeDrags});
     this.setState(prev => {
       return {
         activeDrags: prev.activeDrags - 1,
       };
     });
-    //console.log(this.state.deltaPosition);
   };
   onDrop = (e) => {
-    //this.setState({activeDrags: --this.state.activeDrags});
     this.setState(prev => {
       return {
         activeDrags: prev.activeDrags - 1,
@@ -55,56 +49,34 @@ export default class Notes extends React.Component {
       e.target.classList.remove('hovered');
     }
   };
-  // onDropAreaMouseEnter = (e) => {
-  //   if (this.state.activeDrags) {
-  //     e.target.classList.add('hovered');
-  //   }
-  // }
-  // onDropAreaMouseLeave = (e) => {
-  //   e.target.classList.remove('hovered');
-  // }
-
-  // For controlled component
-  // adjustXPos = (e) => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   const {x, y} = this.state.controlledPosition;
-  //   this.setState({controlledPosition: {x: x - 10, y}});
-  // };
-
-  // adjustYPos = (e) => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   const {controlledPosition} = this.state;
-  //   const {x, y} = controlledPosition;
-  //   this.setState({controlledPosition: {x, y: y - 10}});
-  // };
-
-  // onControlledDrag = (e, position) => {
-  //   const {x, y} = position;
-  //   this.setState({controlledPosition: {x, y}});
-  // };
-
-  // onControlledDragStop = (e, position) => {
-  //   this.onControlledDrag(e, position);
-  //   this.onStop();
-  // };
 
   render() {
     const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+    const {deltaPosition} = this.state;
     const nodeRef = React.createRef(null);
-    const isBorder = toggleBorder;
     return (
-      <Box sx={{ width: '100%', height:'500px', border: isBorder ? '1px solid blue' : 'none'}}>
-        <div style={{height: '500px', width: '100%', position: 'relative', overflow: 'auto', padding: '0', border: isBorder ? '5px solid yellow' : 'none'}}>
-          <Draggable bounds="parent" {...dragHandlers} onDrag={this.handleDrag} nodeRef={nodeRef}>
-            <div className="box" ref={nodeRef}>
-              I can only be moved within my offsetParent.<br /><br />
-              Both parent padding and child margin work properly.
-            </div>
-          </Draggable>
+      <Draggable 
+      bounds="parent" 
+      {...dragHandlers} 
+      onDrag={this.handleDrag} 
+      defaultPosition={{x: Number(deltaPosition.x), y: Number(deltaPosition.y)}} 
+      nodeRef={nodeRef}
+      >
+        <div className="box" ref={nodeRef}>
+          <div>I track my deltas</div>
+          <div>x: {deltaPosition.x.toFixed(0)}, y: {deltaPosition.y.toFixed(0)}</div>
         </div>
-      </Box>
+      </Draggable>
     );
   }
+}
+export default function Notes() {
+  const isBorder = toggleBorder;
+  return(
+    <Box sx={{ width: '100%', height:'500px', border: isBorder ? '1px solid blue' : 'none'}}>
+      <div style={{height: '500px', width: '100%', position: 'relative', overflow: 'auto', padding: '0', border: isBorder ? '5px solid yellow' : 'none'}}>
+        <Note/>
+      </div>
+    </Box>
+  )
 }
