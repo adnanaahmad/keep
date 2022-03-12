f0d7827011c1ffb93a508df5b3fc96a4ac786454
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-03-12 12:59:03 +0500

    header styling fixed

--- src/pages/nav/appHeader/appHeader.js
+++ src/pages/nav/appHeader/appHeader.js
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 
 export default function AppHeader (props) {
     return(
-      <AppBar position="fixed" open={props.open} color="transparent" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <AppBar position="fixed" open={props.open} color="transparent" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 , borderTop:0}} elevation={0} variant="outlined">
         <Toolbar>
           <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'} sx={{width: '100%'}}>
             <Stack direction={'row'} alignItems={'center'}>
