14212c4883d4f4d85bd728e7638702bacf62205d
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-03-18 17:37:18 +0500

    notesdashboard chnaged navigation module

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import lazyWithPreload from './shared/utils/preload';
 const AuthenticationModule = React.lazy(() => import('./modules/auth/auth'));
 const Signup = lazyWithPreload(() => import('./modules/auth/signUp/signup'));
 const Login = lazyWithPreload(() => import('./modules/auth/login/login'));
-const NotesDashboard = React.lazy(() => import('./modules/nav/navigation'));
+const NavigationModule = React.lazy(() => import('./modules/nav/navigation'));
 const AllNotes = lazyWithPreload(() => import('./modules/nav/pages/all/all'));
 const Label = lazyWithPreload(() => import('./modules/nav/pages/label/label'));
 const Archive = lazyWithPreload(() => import('./modules/nav/pages/archive/archive'));
@@ -26,7 +26,7 @@ function App() {
               <Route path="login" element={<Login/>}/>
               <Route path="register" element={<Signup/>}/>
             </Route>
-            <Route path="/notes/*" element={<NotesDashboard data={{AllNotes, Label, Archive, Trash}}/>}>
+            <Route path="/notes/*" element={<NavigationModule data={{AllNotes, Label, Archive, Trash}}/>}>
               <Route path={''} element={<Navigate to='all' replace/>}/>
               <Route path="all" element={<AllNotes />} />
               <Route path="label" element={<Label />} />
