758ddabbcd006a74db6a3b951d36be3f8b7e34ef
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-03-18 13:19:45 +0500

    interceptor added

--- src/shared/utils/interceptor.js
+++ src/shared/utils/interceptor.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = 'http://localhost:8080';
 const clientRequest = axios.create({baseURL});
 
-// apiCall is wrapper function that works as an interceptor
+// apiCall is a wrapper function that works as an interceptor
 
 export const apiCall = ({...options}) => {
     clientRequest.defaults.headers.common.Authorization('Bearer Token');
